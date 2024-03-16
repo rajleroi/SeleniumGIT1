e128b5b717fed33756a4dd4bf0889d31545747d7
rajleroi <163645935+rajleroi@users.noreply.github.com>
2024-03-16 12:46:11 +0000

    Update RajGit.java

--- src/RajGit.java
+++ src/RajGit.java
@@ -1,6 +1,6 @@
 public class RajGit {
         public static void main(String[] args) {
-            int a = 10, b = 20;
-            System.out.println("Sum of a na b is " + (a + b));
+            int a = 10, b = 20,c=40;
+            System.out.println("Sum of a, b,c is " + (a + b+c));
         }
 }
