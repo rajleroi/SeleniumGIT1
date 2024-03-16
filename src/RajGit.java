578ac86acdeae94c2f5512b27d97fac8756e10cb
rajuleroi <rajuleroi@gmail.com>
2024-03-16 12:39:38 +0000

    Intial project2 created and added to GIT

---
+++ src/RajGit.java
@@ -0,0 +1,6 @@
+public class RajGit {
+        public static void main(String[] args) {
+            int a = 10, b = 20;
+            System.out.println("Sum of a na b is " + (a + b));
+        }
+}
